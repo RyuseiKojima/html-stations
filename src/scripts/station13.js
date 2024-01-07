1b9c824f6215ea76bf01e9861bda6f50c1601fb8
Ryusei Kojima <ryusei@Ryuseis-MacBook-Air.local>
2024-01-07 16:36:09 +0900

    0107

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -2,12 +2,19 @@ data = "global data";
 
 // このreturnDataに代入される関数の形をアロー関数に変えて"global data"が表示されるようにしてください
 // ↓関数の中の処理は変更しないでください
-const returnThisData = function () {
+const returnThisData = () => {
+  console.log(this);
   return this.data;
 };
 // ↑関数の中の処理は変更しないでください
+// function returnThisData() {
+//   console.log(this);
+//   return this.data;
+// };
 
 const object = {
+  // 普通の関数：呼び出し時に処理を実行
+  // アロー関数：既に処理を実行済み
   data: "object data",
   func: returnThisData,
 };
--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -1,16 +1,26 @@
 async function getData() {
-  // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
-  // ３秒後にフルネームのリストを表示されるようにしましょう。
-  // 最初から表示されていると、クリアになりません。
   const userList = [
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
+  return await Promise.all(userList.map(buildFullName));
 }
 
-function test() {
-  return
-}
+// 待つ秒数を指定する関数
+function wait(sec) {
+  return new Promise((resolve) => {
+    // setTimeout(時間経過後のプログラム, 開始までの時間[ミリ秒])
+    setTimeout(resolve, sec*1000);
+  });
+};
 
+// フルネームを作成する関数
+async function buildFullName(data) {
+  try {
+    await wait(3); // 3秒止める
+    data.full_name = data.family_name + ' ' + data.first_name;
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
